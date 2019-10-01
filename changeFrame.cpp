9b120370e1d297c2631260df5649f3c71c6ba49c
pf201811281558819 <acchn@live.com>
2019-10-01 20:08:05 +0800

    first step

---
+++ changeFrame.cpp
@@ -0,0 +1,49 @@
+﻿// changeFrame.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <list>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	list<string> l1;
+	string s1;
+	
+	
+	ifstream fin;
+	fin.open("in.txt");
+	
+	while (fin.eof()) {
+		if (fin) {
+			fin >> s1;
+			l1.push_back(s1);
+		}
+
+	}
+	//统计第一行有几个命令
+
+
+	l1.remove("Info");
+	l1.remove("Req:");
+	l1.remove("Ans:");
+
+
+	
+	ofstream fout;
+	fout.open("out.txt");
+	
+	//单帧
+
+	//第一帧
+
+	//连续帧
+	
+	//最后一行的帧命令
+
+	
+	cout << "执行完毕\n";
+}
+
