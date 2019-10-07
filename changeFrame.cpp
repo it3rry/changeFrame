6ada3805e24bd3fa0ac8c0b1022cbf376695cfde
pf201811281558819 <acchn@live.com>
2019-10-07 21:26:51 +0800

    还没有完成，迭代器设计还有缺陷

--- changeFrame.cpp
+++ changeFrame.cpp
@@ -18,7 +18,7 @@ int main()
 	//统计第一行有几个命令
 	ifstream ftest;
 	ftest.open("in.txt");
-	int _num;
+	int _num = 0;
 	while (ftest.eof()) {
 		if (ftest) {
 			if (str != "Ans:") {
@@ -40,6 +40,22 @@ int main()
 	}
 
 	//获取源地址和目标地址
+	list<string>::iterator _getadd = l1.begin();
+	_getadd++;
+	_getadd++;
+	string add11 = *_getadd;
+	_getadd++;
+	string add12 = *_getadd;
+
+	while (*_getadd == "Ans:") {
+		_getadd++;
+	}
+	_getadd++;
+	string add21 = *_getadd;
+	_getadd++;
+	string add22 = *_getadd;
+
+	//处理掉无用的信息
 	l1.remove("Info");
 	l1.remove("Req:");
 	l1.remove("Ans:");
@@ -49,17 +65,32 @@ int main()
 	fout.open("out.txt");
 
 	list<string>::iterator _str = l1.begin();
+	int _sing_frame_num = 4;
+	for (int i = 0; i < _sing_frame_num; i++) {
+		_str++;
+	}
 	
+
 	//单帧
 	//fout << Req << "  "<<  Head << add11 << add12;
 	for (int i = 0; i < _num; i++) {
 		fout << *_str << endl;;
 	}
+	
+	fout << Head << "   " << add11 << add12;
+	while (*_str == "Info") {
+		fout << *_str;
+		_str++;
+	}
+	//单帧补齐
+	fout << endl;
+	
 	//第一帧
-	//fout 
-		//连续帧
 	int _start = 21;
-	//fout << 
+	fout << Ans << "MN " << Head << add21 << add22
+		<< "总数" << *_str++;
+	//连续帧
+
 	//最后一行的帧命令
 
 	
